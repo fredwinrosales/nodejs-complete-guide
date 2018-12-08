496f0eaca8b653f9246113e7f66245024aca5d09
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-08 16:41:13 -0400

    Working with middlewares

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const express = require("express")
 
 const app = express()
 
-app.use((req, res, next) => 
+//app.use((req, res, next) => 
+//{
+//    console.log("In the middleware!")
+//    next()
+//})
+
+//app.use((req, res, next) => 
+//{
+//    console.log("In another middleware!")
+//    res.send("<h1>This is a response!</h1>")
+//})
+
+app.use("/users", (req, res, next) => 
 {
-    console.log("In the middleware!")
-    next()
+    res.send("<h1>This is \"/users\" route!</h1>")
 })
 
-app.use((req, res, next) => 
+app.use("/", (req, res, next) => 
 {
-    console.log("In another middleware!")
-    res.send("<h1>Hello world!</h1>")
+    res.send("<h1>This is \"/\" route!</h1>")
 })
 
 app.listen(3000)
