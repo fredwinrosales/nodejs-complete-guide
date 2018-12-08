890fe3453d04ab193b1c980e4f8a222cab9c9de7
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-08 11:35:28 -0400

    Assignment 1
    - Spin un a Nodejs-driven Server (on port 3000)
    - Handle two Routes: "/" and "/users"
       Return some greeting text on "/"
       Return a list of dummy users (e.g. <ul><li>User 1</li></ul>)
    - Add a form with a "username" <input> to the "/" page and submit a POST request to "/create-user" upon a button click
    - Add the "/create-user" route and parse the incoming data (i.e. the username) and simply log it to the console.

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
 const http = require("http")
 
-const server = http.createServer((req, res) => {
-    
+const server = http.createServer((req, res) => 
+{
+    res.setHeader("Content-type", "text/html")
+    if(req.url == "/" && req.method == "GET")
+    {
+        res.write("<html>")
+        res.write("<head><title>Greeting page</title></head>")
+        res.write("<body>")
+        res.write("<h1>Hello world!</h1>")
+        res.write("<form action=\"/create-user\" method=\"POST\">")
+        res.write("<input type=\"text\" name=\"username\">")
+        res.write("<button type=\"submit\" name=\"submit\">Submit</button>")
+        res.write("</form>")
+        res.write("</body>")
+        res.write("</html>")
+    }
+    else if(req.url == "/users" || req.url == "/users/"  && req.method == "GET")
+    {
+        res.write("<html>")
+        res.write("<head><title>Dummy users page</title></head>")
+        res.write("<body>")
+        res.write("<ul>")
+        res.write("<li>User 1</li>")
+        res.write("<li>User 2</li>")
+        res.write("<li>User 3</li>")
+        res.write("<li>User 4</li>")
+        res.write("</ul>")
+        res.write("</body>")
+        res.write("</html>")
+    }
+    else if(req.url == "/create-user" || req.url == "/create-user/"  && req.method == "POST")
+    {
+        const body = []
+        req.on("data", (chunk) => 
+        {
+            body.push(chunk)
+        })
+        req.on("end", () => 
+        {
+            const parsedBody = Buffer.concat(body).toString()
+            const data = parsedBody.split("&")
+            const username = data[0].split("=")[1]
+            console.log(username)
+        })
+    }
+    return res.end()
 })
 
 server.listen(3000)
