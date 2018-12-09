197590407576b45f23dd6870af25ba65eccca7fa
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 09:00:57 -0400

    Returning a 404 page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 const express = require("express")
 
 const shopRouter = require("./router/shop")
@@ -13,7 +15,7 @@ app.use(shopRouter)
 app.use("/admin", adminRouter)
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found!</h1>")
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 })
 
 app.listen(3000)
