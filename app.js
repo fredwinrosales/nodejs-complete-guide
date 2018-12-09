90d748bea65210b187dd1ec24cb8e612c5abf3a7
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 08:16:57 -0400

    Using Express Router

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 const express = require("express")
+
+const adminRouter = require("./router/admin")
+const shopRouter = require("./router/shop")
+
 const bodyParser = require("body-parser")
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use("/add-product", (req, res, next) => 
-{
-    res.send("<form action=\"/product\" method=\"POST\"><input type=\"text\" name=\"title\"><button type=\"/submit\">Submit</button>")
-})
-
-app.post("/product", (req, res, next) => 
-{
-    console.log(req.body)
-    res.redirect("/")
-})
-
-app.use("/", (req, res, next) => 
-{
-    res.send("<h1>This is \"/\" route!</h1>")
-})
+app.use(shopRouter)
+app.use(adminRouter)
 
 app.listen(3000)
