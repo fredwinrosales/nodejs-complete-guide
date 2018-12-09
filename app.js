58626ff53ccdfa95c17c49734fc9829542db343f
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 14:24:56 -0400

    Implementing Pug

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+app.set("view engine", "pug")
+app.set("views", "views")
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "public")))
 
