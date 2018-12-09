b4e270df6ccd6bfb2cc657bd3fa87a9c05890e49
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 10:01:39 -0400

    Serving Files Statically

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const bodyParser = require("body-parser")
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use(shopRouter)
 app.use("/admin", adminRouter)
