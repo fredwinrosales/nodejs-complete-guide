33872062517a83665af5364115314f42586113d8
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 15:09:56 -0400

    Outputting dynamic content

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.set("views", "views")
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use("/admin", adminRouter.routes)
 app.use(shopRouter)
-app.use("/admin", adminRouter)
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
--- router/shop.js
+++ router/shop.js
@@ -3,12 +3,14 @@ const path = require("path")
 const express = require("express")
 
 const rootDir = require("../util/path")
+const adminData = require("./admin")
 
 const router = express.Router()
 
 router.get("/", (req, res, next) => 
 {
-    res.render("shop")
+    const products = adminData.products
+    res.render("shop", {prods: products, title: "My Shop"})
 })
 
 module.exports = router
