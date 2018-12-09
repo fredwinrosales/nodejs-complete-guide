2a8e850aa8aa9b079f0ca178431aec2254c1d29b
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 08:30:45 -0400

    Filtering paths

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 
-const adminRouter = require("./router/admin")
 const shopRouter = require("./router/shop")
+const adminRouter = require("./router/admin")
 
 const bodyParser = require("body-parser")
 
@@ -10,7 +10,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(shopRouter)
-app.use(adminRouter)
+app.use("/admin", adminRouter)
 
 app.use((req, res, next) => {
     res.status(404).send("<h1>Page not found!</h1>")
