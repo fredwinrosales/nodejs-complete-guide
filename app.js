93d595bcd35f62dc11e1913bbb517d89f5729bdd
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-09 15:26:22 -0400

    Converting HTML files to Pug

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use("/admin", adminRouter.routes)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+    res.status(404).render("404", {title: "404 Not Found"})
 })
 
 app.listen(3000)
--- router/admin.js
+++ router/admin.js
@@ -10,7 +10,7 @@ const products = []
 
 router.get("/add-product", (req, res, next) => 
 {
-    res.sendFile(path.join(rootDir, "views", "add-product.html"))
+    res.render("add-product", {title: "Add Product"})
 })
 
 router.post("/product", (req, res, next) => 
