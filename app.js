6f6179479e7b8349a4796db1337df1a3af03da66
Fredwin Rosales <fredwinrosales@gmail.com>
2018-12-10 07:22:31 -0400

    Working with EJS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
-app.set("view engine", "pug")
+app.set("view engine", "ejs")
 app.set("views", "views")
 
 app.use(bodyParser.urlencoded({extended: false}))
